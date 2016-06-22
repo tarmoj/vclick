7efbe94df3afaf938398e74b84c92fa74323bf01
Tarmo Johannes <tarmo@otsakool.edu.ee>
2016-06-23 01:40:12 +0300

    Fixed #ifdef for android build to find the sound files (server)

--- client/main.cpp
+++ client/main.cpp
@@ -20,8 +20,7 @@ int main(int argc, char *argv[])
 	//engine.rootContext()->setContextProperty("settings", &settings);
 #ifdef Q_OS_ANDROID
 	engine.rootContext()->setContextProperty("installPath", "assets:/"); // take care in .pro file to install the files
-#endif
-#ifdef Q_OS_MACX
+#elif defined(Q_OS_MACX)
     engine.rootContext()->setContextProperty("installPath", "file:///" + QCoreApplication::applicationDirPath() + "/../Resources/sounds/"); // necessary since sounds cannot be played repeatedly from resource
 #else
 	engine.rootContext()->setContextProperty("installPath", "file:///" + QCoreApplication::applicationDirPath() + "/sounds/"); // necessary since sounds cannot be played repeatedly from resource
