b2d08e9cb274012e1146d42e8eb449de43bde08b
Tarmo Johannes <tarmo@otsakool.edu.ee>
2016-07-11 14:30:26 +0300

    Added youtube link to news

--- manage/publishconf.py
+++ manage/publishconf.py
@@ -12,7 +12,7 @@ from pelicanconf import *
 
 SITEURL = 'http://tarmoj.github.io/eclick'
 SITE_URL = SITEURL
-RELATIVE_URLS = False#True
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = '../feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = '../feeds/%s.atom.xml'
