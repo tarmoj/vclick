1782ff9d4f73ea010883911af0752e5b6d772705
Tarmo Johannes <tarmo@otsakool.edu.ee>
2016-07-11 07:46:22 +0300

    Added license to all source files.

--- server/csengine.cpp
+++ server/csengine.cpp
@@ -1,3 +1,23 @@
+/*
+	Copyright (C) 2016 Tarmo Johannes
+	trmjhnns@gmail.com
+
+	This file is part of eClick.
+
+	eClick is free software; you can redistribute it and/or modify it under
+	the terms of the GNU GENERAL PUBLIC LICENSE Version 3, published by
+	Free Software Foundation, Inc. <http://fsf.org/>
+
+	eClick is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with eClick; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+	02111-1307 USA
+*/
 #include "csengine.h"
 #include <QDebug>
 #include <QCoreApplication>
