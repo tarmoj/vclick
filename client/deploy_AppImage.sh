2db2dfe8856d29400db359346bf997b252e987c7
Tarmo Johannes <tarmo@otsakool.edu.ee>
2023-07-30 14:02:04 +0300

    Updated client Linux delploy and desktop file.

--- client/deploy_AppImage.sh
+++ client/deploy_AppImage.sh
@@ -7,6 +7,22 @@
 
 #vt näide lehelt: https://docs.appimage.org/packaging-guide/from-source/native-binaries.html#examples
 
+# qt-plugin page: https://github.com/linuxdeploy/linuxdeploy-plugin-qt
+
+# copy desktop and vclick-cleint.png to bin dir 
+# check .desktop for right paths
+
+# for Qt6 set more parametes...
+
+export QMAKE=/home/tarmo/src/Qt/6.5.1/gcc_64/bin/qmake
+#$QML_MODULES_PATHS
 
 
-linuxdeploy --appdir AppDir --executable=vclick-client --desktop-file=vclick-client.desktop  -i vclick-client.png  --plugin=qt  --output appimage
+export QML_SOURCES_PATHS=../../client
+
+linuxdeploy --appdir AppDir --executable=vclick-client --desktop-file=vclick-client.desktop  -i vclick-client.png  
+
+linuxdeploy-plugin-qt-x86_64.AppImage --appdir AppDir
+
+# somehow --plugin qt does not work, it will not find right qt
+linuxdeploy --appdir AppDir  --output appimage
